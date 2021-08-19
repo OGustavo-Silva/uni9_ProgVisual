684a82c86f453468da385555ad4c359ceaeac00f
Thiago G. Traue <t.traue@gmail.com>
2021-08-18 21:44:36 -0300

    Primeiro Projeto - Hello World

---
+++ Hello World/Hello World/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_World
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btMensagem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Olá " + txtNome.Text + ", sinta-se acolhido(a)! :)");
+            lbResultado.Text = txtNome.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            lbResultado.Text = "";
+        }
+    }
+}

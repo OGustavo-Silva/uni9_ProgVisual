e03fffaf4a647f46b9ca3b3d615179f1c1d331e9
Gustavo Silva <gustavos_barros@hotmail.com>
2021-09-30 20:31:34 -0300

    exercicio caixa eletronico

---
+++ ExercicioCaixaEletronico/ExercicioCaixaEletronico/FrmPrincipal.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ExercicioCaixaEletronico
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaque = new System.Windows.Forms.Label();
+            this.txtSaque = new System.Windows.Forms.TextBox();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.btnSaque = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaque
+            // 
+            this.lblSaque.AutoSize = true;
+            this.lblSaque.Location = new System.Drawing.Point(33, 60);
+            this.lblSaque.Name = "lblSaque";
+            this.lblSaque.Size = new System.Drawing.Size(81, 13);
+            this.lblSaque.TabIndex = 0;
+            this.lblSaque.Text = "Valor de saque:";
+            // 
+            // txtSaque
+            // 
+            this.txtSaque.Location = new System.Drawing.Point(147, 60);
+            this.txtSaque.Name = "txtSaque";
+            this.txtSaque.Size = new System.Drawing.Size(100, 20);
+            this.txtSaque.TabIndex = 1;
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(93, 173);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(120, 95);
+            this.lstResultado.TabIndex = 2;
+            // 
+            // btnSaque
+            // 
+            this.btnSaque.Location = new System.Drawing.Point(147, 123);
+            this.btnSaque.Name = "btnSaque";
+            this.btnSaque.Size = new System.Drawing.Size(75, 23);
+            this.btnSaque.TabIndex = 3;
+            this.btnSaque.Text = "SACAR";
+            this.btnSaque.UseVisualStyleBackColor = true;
+            this.btnSaque.Click += new System.EventHandler(this.btnSaque_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 321);
+            this.Controls.Add(this.btnSaque);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.txtSaque);
+            this.Controls.Add(this.lblSaque);
+            this.Name = "FrmPrincipal";
+            this.Text = "Caixa Eletronico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaque;
+        private System.Windows.Forms.TextBox txtSaque;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.Button btnSaque;
+    }
+}
+
---
+++ ExercicioCaixaEletronico/ExercicioCaixaEletronico/FrmPrincipal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ Desenvolva uma aplicação WinForms com C# que seja capaz de simular o saque em um CAIXA ELETRÔNICO, com notas de 2, 5, 10, 20, 50 e 100. 
+
+Vocês devem colar na caixa abaixo apenas o algoritmo utilizado. Não é preciso colar o programa inteiro.
+
+Lembre-se que os únicos valores impossíveis de serem sacados é 1 e 3. 
+
+Neste caixa não há moedas. É preciso exibir a quantidade de cada nota a ser entregue ao cliente.
+
+
+Exemplos: 
+Valor do saque: 70
+Resultado:
+ - Uma nota de 50
+ - Uma nota de 20
+---------------------------------
+Valor do saque: 101
+ - Uma nota de 50
+ - Duas notas de 20
+ - Uma nota de 5
+ - Três notas de 2
+ */
+
+namespace ExercicioCaixaEletronico
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaque_Click(object sender, EventArgs e)
+        {
+            int valorSaque = int.Parse(txtSaque.Text);
+            int[] notas = { 100, 50, 20, 10, 5, 2 };
+            int contadorNota = 0;
+            lstResultado.Items.Clear();
+
+            if(valorSaque == 1 || valorSaque == 3)
+            {
+                lstResultado.Items.Add("Não pode sacar " + valorSaque);
+            }
+
+            else
+            {
+
+                foreach (int nota in notas)
+                {
+                    contadorNota = 0;
+                    while (valorSaque >= nota &&
+                            (valorSaque - nota != 1) && (valorSaque - nota != 3)
+                          )
+                    {
+                        valorSaque -= nota;
+                        contadorNota++;
+                    }
+                    if (contadorNota != 0) {
+                        lstResultado.Items.Add("Notas de " + nota + ": " + contadorNota);
+                    }
+
+                }
+                
+            }
+
+        }
+    }
+}
